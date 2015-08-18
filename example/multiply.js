5241f29b53620ab848c03dcaeefface0289ff6d4
panchishin <anton.panchishin@gmail.com>
2015-08-18 10:15:04 -0400

    Improve example

--- example/multiply.js
+++ example/multiply.js
@@ -1,34 +1,34 @@
 
 console.log("\nTest the learning capabilities of an Artificial Neural Net");
 console.log("\nTrain a neural net to predict the outcome of this calculation");
-console.log(" A * B / 2  , where A and B are a decimal between 0 and 1.\n");
+console.log(" A * B , where A and B are a decimal between 0.15 and 0.85.\n");
+
 
 
 var config = {
 	inputs 				: 2 , 
-	hiddenLayers 		: 1 , 
-	hiddenNodesPerLayer : 10 ,
-	outputs				: 1 ,
-	learningRate		: 1
+	outputs				: 1
 }
 
 var neuralnet = require("../neuralnet.js")(config)
 
-function doOneTraining() {
-	var a = Math.random()
-	var b = Math.random()
+function random() { return (Math.random() * 0.7) + 0.15 }
 
-	neuralnet.train( [ a , b , -1 ] , [ a * b / 2 ] )
+function doOneTraining() {
+	var a = random()
+	var b = random()
+
+	neuralnet.train( [ a , b ] , [ a * b ] )
 }
 
 function doOnePredictionAndGetError() {
-	var a = Math.random()
-	var b = Math.random()
+	var a = random()
+	var b = random()
 
-	return neuralnet.predict( [ a , b , -1 ] )[0] - (a * b / 2)
+	return neuralnet.predict( [ a , b ] )[0] - (a * b )
 }
 
-var TRAINING = 100
+var TRAINING = 1000
 
 function calculateAverageError() {
 	var SAMPLES = 100
@@ -40,12 +40,42 @@ function calculateAverageError() {
 	return Math.pow( error / SAMPLES , 0.5);
 }
 
-console.log(Math.round( calculateAverageError() * 100 ) + "% : The initial RMS error" )
+console.log("\nBefore training.  Here are some example attempts of the NeuralNet:\n");
+
+for(var count=0 ; count<5 ; count++) {
+	example()
+}
+
+console.log("\n=== Training starts ===");
 for( var rounds = 0 ; rounds < 10 ; rounds++ ) {
 	for( var count = 0 ; count < TRAINING ; count ++) {
 		doOneTraining()
 	}
-	console.log(Math.abs(Math.round( calculateAverageError() * 100 )) + "% : RMS error after " + TRAINING * (rounds + 1) + " training sessions")
+	process.stdout.write(".")
+}
+console.log("\n=== Training ends ===");
+
+console.log("\nTraining complete.  Here are some example attempts of the NeuralNet:\n")
+
+
+function example() {
+
+	function print(num) {
+		return "'"+Math.round(num * 100)+"'"
+	}
+
+	var a = random()
+	var b = random()
+
+	var predicted = neuralnet.predict( [ a , b ] )
+	var expected = (a * b)
+
+	console.log("Given A = "+print(a)+" and B = "+print(b)+
+		" and an expected result of (A x B) of " +print(expected)+
+		", the NN got " + print(predicted) + " , an error of "+print(Math.abs(predicted-expected)))
+
 }
 
-console.log()
+for(var count=0 ; count<5 ; count++) {
+	example()
+}
